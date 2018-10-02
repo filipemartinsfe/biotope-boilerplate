6c0959341a13a296f7bfe899e8dfade22effa515
Filipe Martins <filipe.martins@virtual-identity.com>
2018-10-02 15:27:12 +0100

    Update Advanced1 to keep Phase 1 API CALL

--- src/components/page/ts/index.page.ts
+++ src/components/page/ts/index.page.ts
@@ -1,5 +1,6 @@
   /// <reference types="jquery"/>
   import data from '../../page/scaffolding/pageContent';
+  import * as contentful from 'contentful';
   (function(jquery: JQueryStatic, window: any, document: any) {
 
     class Plugin {
@@ -15,19 +16,20 @@
       }
 
       public init() : void {
-        data.cards.forEach((el:any) => {
-          document.getElementsByClassName('cards')[0].innerHTML +=
-            `<div 
-             class="cards__topic" 
-             style="background: linear-gradient(to bottom, rgba(0,0,0,.75),rgba(0,0,0,0), rgba(0,0,0,.88)),
-             url('${el.imgSrc}') no-repeat center/cover;">
-              <div class="cards__date">${el.date}</div>
-              <div class="cards__content">
-                <h2 class="cards__title"> ${el.title} </h2>
-                <p class="cards__description"> ${el.description} </p>
-              </div>
-            </div>`
-        });
+
+        // data.cards.forEach((el:any) => {
+        //   document.getElementsByClassName('cards')[0].innerHTML +=
+        //     `<div 
+        //      class="cards__topic" 
+        //      style="background: linear-gradient(to bottom, rgba(0,0,0,.75),rgba(0,0,0,0), rgba(0,0,0,.88)),
+        //      url('${el.imgSrc}') no-repeat center/cover;">
+        //       <div class="cards__date">${el.date}</div>
+        //       <div class="cards__content">
+        //         <h2 class="cards__title"> ${el.title} </h2>
+        //         <p class="cards__description"> ${el.description} </p>
+        //       </div>
+        //     </div>`
+        // });
       }
     }
 
